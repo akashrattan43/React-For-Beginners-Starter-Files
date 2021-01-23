2889d480527b477ceebfa4df70c464c05442a50e
akash rattan <akashrattan43@Gmail.com>
2021-01-22 18:36:40 -0800

    prop

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -7,14 +7,14 @@ class Header extends React.Component {
         <h1>
           Catch
           <span className="ofThe">
-          <span className="of">of</span>
-          <span className='the'>The</span>
+            <span className="of">of</span>
+            <span className="the">The</span>
           </span>
           Day
         </h1>
 
         <h3 className="tagline">
-          <span>Fresh Daily</span>
+          <span>{this.props.tagline}</span>
         </h3>
       </header>
     );
